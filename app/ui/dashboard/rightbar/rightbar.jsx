315800b380e9a9807f598f31b8a64eac378150c3
Milad Joodi <winbeta.com@gmail.com>
2024-02-22 23:51:58 +0330

    ty

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,18 +1,48 @@
 import Image from "next/image";
 import styles from "./rightbar.module.css";
+import { MdPlayCircleFilled, MdReadMore } from "react-icons/md";
 
 const Rightbar = () => {
     return (
         <div className={styles.container}>
+            {/* one */}
+            <div className={styles.item}>
+                <div className={styles.bgContainer}>
+                    <Image src="/astronaut.png" alt="" fill />
+                </div>
+                <div className={styles.texts}>
+                    <span className={styles.notification}>🔥 Available Now</span>
+
+                    <h3 className={styles.title}>how to use the new version of the admin dashboard</h3>
+
+                    <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+                    
+                    <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur est</p>
+
+                    <button className={styles.button}>
+                        <MdPlayCircleFilled />
+                        Watch
+                    </button>
+                </div>
+            </div>
+            {/* tow */}
             <div className={styles.item}>
                 <div className="bgContainer">
                     <Image src="/astronaut.png" alt="" fill />
                 </div>
                 <div className={styles.texts}>
                     <span className={styles.notification}>🔥 Available Now</span>
-                    <h3>how to use the new version of the admin dashboard</h3>
-                    <span>Takes 4 minutes to learn</span>
-                    <p>lorem</p>
+
+                    <h3 className={styles.title}>how to use the new version of the admin dashboard</h3>
+
+                    <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+                    
+                    <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem pariatur est</p>
+
+                    <button className={styles.button}>
+                        <MdReadMore />
+                        Learn
+                    </button>
                 </div>
             </div>
         </div>
