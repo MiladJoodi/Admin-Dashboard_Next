4be77ed63d8ea5e3118f4da2545308459bd17d76
Milad Joodi <winbeta.com@gmail.com>
2024-02-22 23:43:30 +0330

    lrema

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
 import styles from "./rightbar.module.css";
 
 const Rightbar = () => {
     return (
         <div className={styles.container}>
-            Rightbar
+            <div className={styles.item}>
+                <div className="bgContainer">
+                    <Image src="/astronaut.png" alt="" fill />
+                </div>
+                <div className={styles.texts}>
+                    <span className={styles.notification}>🔥 Available Now</span>
+                    <h3>how to use the new version of the admin dashboard</h3>
+                    <span>Takes 4 minutes to learn</span>
+                    <p>lorem</p>
+                </div>
+            </div>
         </div>
     );
 }
