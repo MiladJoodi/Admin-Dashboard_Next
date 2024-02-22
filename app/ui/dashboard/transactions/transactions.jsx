105a3fda5bb1f461fa43aeedabcfd1576e43e8ba
Milad Joodi <winbeta.com@gmail.com>
2024-02-22 22:40:42 +0330

    ty

--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,9 +1,105 @@
+import Image from "next/image";
 import styles from "./transactions.module.css";
 
 const Transactions = () => {
     return (
         <div className={styles.container}>
-            Transactions
+            <h2>Latest Transactions</h2>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Status</td>
+                        <td>Date</td>
+                        <td>Amount</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* transaction row */}
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image
+                                    src="/noavatar.png"
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.userImage}
+                                />
+                                John Doe
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.done}`}>Pending</span>
+                        </td>
+                        <td>14.02.2024</td>
+                        <td>$3.200</td>
+                    </tr>
+
+                    {/* transaction row */}
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image
+                                    src="/noavatar.png"
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.userImage}
+                                />
+                                John Doe
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.cancelled}`}>Pending</span>
+                        </td>
+                        <td>14.02.2024</td>
+                        <td>$3.200</td>
+                    </tr>
+
+                    {/* transaction row */}
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image
+                                    src="/noavatar.png"
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.userImage}
+                                />
+                                John Doe
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.pending}`}>Pending</span>
+                        </td>
+                        <td>14.02.2024</td>
+                        <td>$3.200</td>
+                    </tr>
+
+                    {/* transaction row */}
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image
+                                    src="/noavatar.png"
+                                    alt=""
+                                    width={40}
+                                    height={40}
+                                    className={styles.userImage}
+                                />
+                                John Doe
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.pending}`}>Pending</span>
+                        </td>
+                        <td>14.02.2024</td>
+                        <td>$3.200</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
