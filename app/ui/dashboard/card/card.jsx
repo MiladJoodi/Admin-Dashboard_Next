72404b30aa763609c411492ec298ae37db9637d5
Milad Joodi <winbeta.com@gmail.com>
2024-02-22 22:08:34 +0330

    cont

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,9 +1,18 @@
+import { MdSupervisedUserCircle } from "react-icons/md";
 import styles from "./card.module.css";
 
 const Card = () => {
     return (
         <div className={styles.container}>
-            Card
+            <MdSupervisedUserCircle />
+            <div className={styles.texts}>
+                <span className={styles.title}>Total Users</span>
+                <span className={styles.number}>10.273</span>
+                <span className={styles.detail}>
+                    <span className={styles.positive}>12%</span>
+                    more than previous week
+                </span>
+            </div>
         </div>
     );
 }
