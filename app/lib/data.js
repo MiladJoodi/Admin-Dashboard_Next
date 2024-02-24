7d8fb6e06430637a647ad9577741592b488406ae
Milad Joodi <winbeta.com@gmail.com>
2024-02-24 20:09:00 +0330

    0

--- app/lib/data.js
+++ app/lib/data.js
@@ -24,14 +24,13 @@ export const fetchUsers = async (q, page) => {
 
 //FETCH SINGLE USER
 export const fetchUser = async (id) => {
-
   try {
     connectToDB();
     const user = await User.findById(id)
     return user;
   } catch (err) {
     console.log(err);
-    throw new Error("Failed to fetch users!");
+    throw new Error("Failed to fetch user!");
   }
 };
 
@@ -51,5 +50,18 @@ export const fetchProducts = async (q, page) => {
   } catch (err) {
     console.log(err);
     throw new Error("Failed to fetch products!");
+  }
+};
+
+//FETCH SINGLE PRODUCT
+export const fetchProduct= async (id) => {
+
+  try {
+    connectToDB();
+    const product = await Product.findById(id)
+    return product;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch product!");
   }
 };
