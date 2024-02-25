b08618bcb91a7d85d2c82e9a8e15583a936cb375
Milad Joodi <winbeta.com@gmail.com>
2024-02-25 20:54:02 +0330

    0

--- app/authconfig.js
+++ app/authconfig.js
@@ -7,12 +7,12 @@ export const authConfig = {
       authorized({ auth, request }) {
         const isLoggedIn = auth?.user;
         const isOnDashboard = request.nextUrl.pathname.startsWith("/dashboard");
-        // if (isOnDashboard) {
-        //   if (isLoggedIn) return true;
-        //   return false;
-        // } else if (isLoggedIn) {
-        //   return Response.redirect(new URL("/dashboard", request.nextUrl));
-        // }
+        if (isOnDashboard) {
+          if (isLoggedIn) return true;
+          return false;
+        } else if (isLoggedIn) {
+          return Response.redirect(new URL("/dashboard", request.nextUrl));
+        }
         return true;
       },
     },
