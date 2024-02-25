85ac56c907088244598f8ae6b2a91fc0a67af036
Milad Joodi <winbeta.com@gmail.com>
2024-02-25 13:13:34 +0330

    password

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,13 +1,14 @@
 import styles from "@/app/ui/login/login.module.css";
+import { authenticate } from "../lib/data";
 
 
 const LoginPage = () => {
     return (
         <div className={styles.container}>
-            <form action="" className={styles.form}>
+            <form action={authenticate} className={styles.form}>
                 <h1>Login</h1>
-                <input type="text" placeholder="username" />
-                <input type="password" placeholder="password" />
+                <input type="text" placeholder="username" name="username" />
+                <input type="password" placeholder="password" name="password" />
                 <button>Login</button>
             </form>
 
